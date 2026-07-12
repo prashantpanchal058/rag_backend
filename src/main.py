aaf01721c9462ae27658609ba3d56b71bef143e7
Prashant-panchal05 <pp2575676@gmail.com>
2026-07-13 01:54:33 +0530

    change url

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # ✅ MUST be before routes
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",],
+    allow_origins=["https://rag-frontend-kappa.vercel.app/",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
